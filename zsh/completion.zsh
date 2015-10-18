483fbdb94b855daa852a8d9225bad1fd9950028f
dbaq <didier.baquier@myerp.com>
2015-10-17 19:08:02 -0700

    zsh: update completion configuration

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,9 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# activate menu selection
+zstyle ':completion:*' menu select
+
+# let's use the tag name as group name
+zstyle ':completion:*' group-name ''
