5c27feb45d4719f1742be97de722cffce7c5198a
dbaq <didier.baquier@gmail.com>
2015-10-17 18:21:14 -0700

    add my git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,15 +5,3 @@ if (( $+commands[hub] ))
 then
   alias git=$hub_path
 fi
-
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
