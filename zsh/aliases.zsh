773a412fc24e2a495911ac464382b16b493a0671
dbaq <didier.baquier@myerp.com>
2015-10-17 18:33:06 -0700

    add some alias to zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias reload!='. ~/.zshrc'
+
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias ll='ls -la'
+alias tailf='tail -f'
+alias sql='mysql -u root'
