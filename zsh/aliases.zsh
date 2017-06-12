05a6472d58699b66b61d5bd7f2bdd5f2bd1a8375
Didier BAQUIER <didier.baquier@oneup.com>
2017-06-12 16:30:15 -0700

    update path + alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,8 @@ alias .....='cd ../../../../'
 alias ll='ls -la'
 alias tailf='tail -f'
 alias sql='mysql -u root'
-
+alias pg='pgcli postgresql://postgres@127.0.0.1'
 
 export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH="$HOME/.node/bin:$PATH"
+
