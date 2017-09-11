50f696f81c3110a18e4f8a1e8f4509493cd0e7be
dbaq <didier.baquier@gmail.com>
2017-09-11 11:53:31 -0700

    add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,9 @@ alias ll='ls -la'
 alias tailf='tail -f'
 alias sql='mysql -u root'
 alias pg='pgcli postgresql://postgres@127.0.0.1'
+alias oneup='cd ~/oneup/oneup.com'
+alias resque='ssh daemon-01 -N -L 3001:0.0.0.0:5678 -v'
+
 
 export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH="$HOME/.node/bin:$PATH"
