88545e445813b039c6e2a43ca26a0d579d416733
Didier Baquier <didier.baquier@gmail.com>
2017-09-11 11:56:06 -0700

    fix typo

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,7 +8,7 @@ alias ll='ls -la'
 alias tailf='tail -f'
 alias sql='mysql -u root'
 alias pg='pgcli postgresql://postgres@127.0.0.1'
-alias oneup='cd ~/oneup/oneup.com'
+alias oneup='cd ~/oneup.com/oneup'
 alias resque='ssh daemon-01 -N -L 3001:0.0.0.0:5678 -v'
 
 
